400a428d910e84ba8e00fc2fd51716a862c52cc0
mohit2152sharma <mohitlakshya@gmail.com>
2022-07-13 00:55:58 +0530

    adding https and handling prod urls

--- saral_utils/utils/env.py
+++ saral_utils/utils/env.py
@@ -20,16 +20,19 @@ def get_env_var(env: str) -> str:
     return env_var
 
 def create_env_api_url(url: str) -> str:
-    """depending on the environment variable returns the api url. Attaches the environment variable at the beggining of api url e.g. `test-deregister.saral.club`. But for prod it's without env i.e. `deregister.saral.club`
+    """depending on the environment variable returns the api url. Attaches the environment variable at the beggining of api url e.g. `https://test-deregister.saral.club`. But for prod it's without env i.e. `https://deregister.saral.club`
 
     Args:
         url (str): URL string
 
     Returns:
-        str: `test-deregister.saral.club` for test/stg environment and `deregister.saral.club` for prod environments
+        str: `https://test-deregister.saral.club` for test/stg environment and `https://deregister.saral.club` for prod environments
     """
+    if url.startswith('http'):
+        url = url.split('//')[-1]
+
     env = get_env_var('MY_ENV')
     if env != 'prod':
         url = f'{env}-{url}'
-    return url
+    return f'https://{url}'
 
